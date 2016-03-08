6cd4584cb9b689f92655152aaeafe223bce4121b
SamHeutmaker <samheutmaker@gmail.com>
2016-03-08 15:31:11 -0800

    fuck it

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('build'));
 
-// app.get('*', (req, res) => {
-// 	res.status(404).json({
-// 		msg: 'Not Found.'
-// 	});
-// });
-
-app.listen(PORT, () => console.log('PORT ' + PORT));
+app.listen(PORT, () => {
+  console.log('Server up on port ' + PORT);
+})
