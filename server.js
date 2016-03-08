13747538679385247623361f63703e568fa889db
SamHeutmaker <samheutmaker@gmail.com>
2016-03-08 15:18:56 -0800

    Removed catch all

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/build'));
 
-app.get('*', (req, res) => {
-	res.status(404).json({
-		msg: 'Not Found.'
-	});
-});
+// app.get('*', (req, res) => {
+// 	res.status(404).json({
+// 		msg: 'Not Found.'
+// 	});
+// });
 
 app.listen(PORT, () => console.log('PORT ' + PORT));
